4d1af3b817db184719146bdfac2ed31c272032dc
heifade <heifade@126.com>
2018-06-06 17:16:31 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,10 @@ export class BuildertoolsConfig {
   publicPath?: string = ""; // 资源URL前缀
   noParse?: RegExp | RegExp[] | ((content: string) => boolean); // 忽略对部分没有模块化的文件的解析
   /*
-    1、"jQuery"
+    1、"jQuery" // jQuery不打包到Chunk，可以通过 require("jQuery") 导入
     2、["jQuery", "lodash"]
     3、{
-      jquery: "jQuery",
+      jquery: "jQuery", // 全局变量jQuery,可以通过 require("jquery") 导入
       lodash: "lodash"
     }
     4、[{
@@ -36,5 +36,10 @@ export class BuildertoolsConfig {
       lodash: "lodash"
     }]
   */
-  externals?: string | string[] | ExternalsObjectElement | ExternalsObjectElement[]; // 不用打包到Chunk的库
+  externals?: string | string[] | ExternalsObjectElement | ExternalsObjectElement[]; // 不打包到Chunk的库
+
+  // 代理到后端服务接口
+  proxy?: {
+    [key: string]: string;
+  };
 }

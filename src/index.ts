95e1f43cdaa4a8716f52ee6ffc82a6ba3bbc8319
heifade <heifade@126.com>
2018-06-06 15:55:49 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+interface ExternalsObjectElement {
+  [key: string]: boolean | string;
+}
+
 export class BuildertoolsConfig {
   mode?: "development" | "production" = "production";
   /*
@@ -19,4 +23,17 @@ export class BuildertoolsConfig {
   entry?: string | string[] | { [name: string]: string | string[] } = { index: "./src/index" };
   port?: number = 8080; // 运行端口号
   noParse?: RegExp | RegExp[] | ((content: string) => boolean); // 忽略对部分没有模块化的文件的解析
+  /*
+    1、"jQuery"
+    2、["jQuery", "lodash"]
+    3、{
+      jquery: "jQuery",
+      lodash: "lodash"
+    }
+    4、[{
+      jquery: "jQuery",
+      lodash: "lodash"
+    }]
+  */
+  externals?: string | string[] | ExternalsObjectElement | ExternalsObjectElement[];
 }

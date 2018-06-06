1acf7d35570e9cd803ee34573676f9b8d0d4d521
heifade <heifade@126.com>
2018-06-06 08:40:26 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 export class BuildertoolsConfig {
   mode?: "development" | "production" = "production";
   /*
-    entry: "./src/index.js",
-    entry: {
+    1、entry: "./src/index.js",
+    2、entry: [
+      "./src/index.js",
+      "./src/main.js"
+    ]
+    3、entry: {
       index: "./src/index.js"
     },
-    entry: {
+    4、entry: {
       index: [
         "./src/index.js",
         "./src/main.js"
       ]
     }
   */
-  entry?: string | { [name: string]: string | string[] } = { index: "./src/index" };
+  entry?: string | string[] | { [name: string]: string | string[] } = { index: "./src/index" };
   port?: number = 8080;
 }
